d27e7545e821e2d20daf969435709e6f5083bd4c
Brian Borealis & Logan Wu <student@epicodus.com>
2015-08-03 10:37:23 -0700

    Add constructor.

--- Car.php
+++ Car.php
@@ -6,26 +6,17 @@ class Car
     public $miles;
 }
 
+function __construct($car_model, $car_price, $car_miles)
+{
+  $this->make_model = $car_model;
+  $this->price = $car_price;
+  $this->miles = $car_miles;
+}
 
-$porsche = new Car();
-$porsche->make_model = "2014 Porsche 911";
-$porsche->price = 114991;
-$porsche->miles = 7864;
-
-$ford = new Car();
-$ford->make_model = "2011 Ford F450";
-$ford->price = 55995;
-$ford->miles = 14241;
-
-$lexus = new Car();
-$lexus->make_model = "2013 Lexus RX 350";
-$lexus->price = 44700;
-$lexus->miles = 20000;
-
-$mercedes = new Car();
-$mercedes->make_model = "Mercedes Benz CLS550";
-$mercedes->price = 39900;
-$mercedes->miles = 37979;
+$porsche = new Car("2014 Porsche 911", 114991, 7864);
+$ford = new Car("2011 Ford F450", 55995, 14241);
+$lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
 
 $cars = array($porsche, $ford, $lexus, $mercedes);
 
